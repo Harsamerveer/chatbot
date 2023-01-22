a5c6f7b16f4bce575ebe8d37344c1d85aa059d1f
Harsamerveer <harsamerveersingh@gmail.com>
2023-01-22 13:33:08 -0800

    New File ChatBot.html

--- MyChatbot.py
+++ MyChatbot.py
@@ -1,23 +1,30 @@
 # Test Cases
 # Check want does bot want from this point
-# Display projects or education or accompliments or work experience
-
+# Choose from one of the following topics or type your question.
+# Provide Hyperlink sections and redirect to the following sections
+# Section as follows: Projects or Education or Accompliments or Work Experience
 
 responses = {
     "hi": "Hello!",
     "how are you": "I'm good, thanks for asking!",
     "bye": "Goodbye!",
+    "resume": "<a href='https://harsamerveersingh.w3spaces.com/'>Resume</a>",
+    "portfolio": "<a href='https://harsamerveersingh.w3spaces.com/'>Portfolio</a>",
+    "help": "You can ask me about my resume or portfolio, or simply say hi or bye.",
+    "stop": "Exiting chatbot."
 }
 
 def chatbot():
+    print("Welcome to the chatbot. Type 'help' for a list of commands.")
     while True:
-        user_input = input("You: ")
-        if user_input.lower() in responses:
+        user_input = input("You: ").lower()
+        if user_input in responses:
+            if user_input == "stop":
+                print("Bot: " + responses[user_input])
+                break
             print("Bot: " + responses[user_input])
-        elif user_input.lower() == "bye":
-            break
         else:
-            print("Bot: I'm sorry, I didn't understand your input.")
+            print("Bot: I'm sorry, I didn't understand your input. Type 'help' for a list of commands.")
 
 chatbot()
 
